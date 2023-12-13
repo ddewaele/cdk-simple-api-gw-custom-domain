55f531c582c4821a560ad78ac6acae0b2c448765
ddewaele <ddewaele@gmail.com>
2023-12-13 12:33:21 +0100

    Initial commit

---
+++ test/cdk-simple-api-gw-custom-domain.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkSimpleApiGwCustomDomain from '../lib/cdk-simple-api-gw-custom-domain-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-simple-api-gw-custom-domain-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkSimpleApiGwCustomDomain.CdkSimpleApiGwCustomDomainStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
